71482acc580eeadf0f363dd3c3c175a8a8d311d7
sabrinaflorenco <sabrinaflorenco@bol.com.br>
2023-10-11 15:48:25 -0300

    feat:adicionado arquivo html e js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let h1 = document.querySelector('h1');
+h1.innerText = 'Título';
+
+let a = document.querySelector('a');
+a.innerText = 'Prozed';
+let ul = document.querySelector('ul');
+ul.innerHTML = `<li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>`;
+let ol = document.querySelector('#lista-ordenada');
+ol.innerHTML = `<li><a href="https://www.google.com">Google</a></li>
+               <li><a href="https://prozeducacao.com.br">Prozeducação</a></li>
+               <li><a href="https://www.globo.com">Globo</a></li>`;
